670b0dfdbe7d11e75180a42375c0170667f4d4be
zialdiansyah <ziaulhafizda@gmail.com>
2021-04-04 15:54:42 +0700

    update serviceworker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,16 +1,19 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
-    '/offline',
-    '/css/app.css',
+    // '/offline',
+    '/css/dashboard.css',
+    '/css/fixlanding.css',
     '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    '/js/dashboard.js',
+    '/icon.png',
+    // '/images/icons/icon-72x72.png',
+    // '/images/icons/icon-96x96.png',
+    // '/images/icons/icon-128x128.png',
+    // '/images/icons/icon-144x144.png',
+    // '/images/icons/icon-152x152.png',
+    // '/images/icons/icon-192x192.png',
+    // '/images/icons/icon-384x384.png',
+    // '/images/icons/icon-512x512.png',
 ];
 
 // Cache on install
