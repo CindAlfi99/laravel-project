453128589c3278fa529efad90c935fdf94ef7d35
zialdiansyah <ziaulhafizda@gmail.com>
2021-04-04 15:45:57 +0700

    tambah pwa

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,52 +1,67 @@
-@extends('layout.about')
-
-@section('title', 'about')
-@section('head')
+<!doctype html>
+<html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
-    <title>@yield('title')</title>
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link href="{{asset('css/fixlanding.css')}}" rel="stylesheet">
+  <title>Rumah Laundry 381 - About</title>
+  @laravelPWA
 </head>
-@endsection
 
 <body>
-    @section('main')
-    <h1>About</h1>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar w/ text</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+  <section id="navbar">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="/"><span>R'L381</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-            </ul>
-            <span class="navbar-text">
-                Navbar text with an inline element
-            </span>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="{{url('/about')}}">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Contact</a>
+            </li>
+          </ul>
         </div>
+        <div class="collapse navbar-collapse navv" id="navbarNav">
+          <ul class="navbar-nav ml-md-auto">
+            <li class="nav-item mr-md-2 mb-2 mb-md-0">
+              <button class="nav-link btn btn-light btn-custom" href="#" data-toggle="modal" data-target="#modalSignIn"><span>Sign In</span></button>
+            </li>
+            <li class="nav-item">
+              <button class="nav-link btn btn-light btn-custom" href="/register" data-toggle="modal" data-target="#modalSignUp"><span>Sign Up</span></button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-    @endsection
+    <!-- Optional JavaScript; choose one of the two! -->
+  </section>
 
-
-    @section('script')
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-    @endsection
+  <section class="container mt-5">
+      <h1 class="h3">About Us</h1>
+      <article>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur incidunt odio impedit deleniti suscipit vitae temporibus officia eveniet dolorum mollitia. Ipsam ducimus quos, illum neque voluptatem possimus nulla? Ullam, vero!
+      </article>
+  </section>
+  <footer class="my-4 text-center">
+    <p class="mb-0">&copy; 2021 Rumah Laundry 381, Palembang, Indonesia.</p>
+  </footer>
 </body>
 
 </html>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -10,7 +10,8 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <link href="{{asset('css/fixlanding.css')}}" rel="stylesheet">
-  <title>Welcome,Rumah Laundry 381</title>
+  <title>Rumah Laundry 381</title>
+  @laravelPWA
 </head>
 
 <body>
